d3b7dfac637fd53cdff1830200c9e9a1339adc28
Kevroo Nguyen <kevroonguyen@Kevroos-MacBook-Pro.local>
2020-03-28 21:45:36 -0700

    Commit 9

--- test_main.py
+++ test_main.py
@@ -24,7 +24,6 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    assert False
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
@@ -35,7 +34,7 @@ def test_auth(client):
     response = client.post('/auth', 
                            data=json.dumps(body),
                            content_type='application/json')
-    assert False
+
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
